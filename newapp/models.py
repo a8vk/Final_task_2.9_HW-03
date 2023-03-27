6a29eb0d583c4ad2cabd4bd73394bcc4a79f9e73
a8vk <a8vk@ya.ru>
2023-03-27 23:35:28 +1000

    added empty project

---
+++ newapp/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+# Класс Автор
+class Author(models.Model):
+    pass
+
+
+# Класс Категория
+class Category(models.Model):
+    pass
+
+
+# Класс Сообщение
+class Post(models.Model):
+    pass
+
+
+# Класс Категория сообщения
+class PostCategory(models.Model):
+    pass
+
+
+# Класс Комментарий
+class Comment(models.Model):
+    pass
