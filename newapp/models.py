dc1656ba3de19292699c7bc96fc58bde8e61d6fc
a8vk <a8vk@ya.ru>
2023-03-28 00:36:06 +1000

    added models Author,Category,Post,PostCategory,Comment

--- newapp/models.py
+++ newapp/models.py
@@ -1,26 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Класс Автор
 class Author(models.Model):
-    pass
+    authorUser = models.OneToOneField(User, on_delete=models.CASCADE)
+    ratingAuthor = models.SmallIntegerField(default=0)  # поле рейтинг автора
+
+    def update_rating(self):  # метод обновление рейтинга
+        pass
 
 
 # Класс Категория
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=64, unique=True)  # максимальную длину рекомендуется указывать 2 в n степени
 
 
 # Класс Сообщение
 class Post(models.Model):
-    pass
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)  # поле автор
+    NEWS = 'NW'
+    ARTICLE = 'AR'
+    # поле выбора
+    CATEGORY_CHOICES = (
+        (NEWS, 'Новость'),
+        (ARTICLE, 'Статья')
+    )
+    categoryType = models.CharField(max_length=2, choices=CATEGORY_CHOICES, default=ARTICLE)
+    dateCreation = models.DateTimeField(auto_now_add=True)  # Автоматически добавлять дату и время создания
+    postCategory = models.ManyToManyField(Category, through='PostCategory')
+    title = models.CharField(max_length=128)
+    text = models.TextField()
+    rating = models.SmallIntegerField(default=0)
+
+    def like(self):
+        pass
+
+    def dislike(self):
+        pass
 
 
 # Класс Категория сообщения
 class PostCategory(models.Model):
-    pass
+    postThrough = models.ForeignKey(Post, on_delete=models.CASCADE)
+    categoryThrough = models.ForeignKey(Post, on_delete=models.CASCADE)
 
 
 # Класс Комментарий
 class Comment(models.Model):
-    pass
+
+    def like(self):
+        pass
+
+    def dislike(self):
+        pass
